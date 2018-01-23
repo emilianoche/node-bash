6c1c084bc78bdbfd7a51137f4eb02a6de0dd416d
emilianoche <chequeremiliano@gmail.com>
2018-01-23 12:35:42 -0300

    hasta la refactorizacion esta ok

--- bash.js
+++ bash.js
@@ -4,8 +4,12 @@ var fs = require('fs')
 process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-
     var str = data.toString().trim();
     var cmd = str.split(' ');
-    command[cmd[0]](str);
+    command[cmd[0]](str, done);
 });
+
+var done =function(output){
+    process.stdout.write(output);
+    process.stdout.write('\nprompt > ')
+}
--- commands.js
+++ commands.js
@@ -1,46 +1,150 @@
 var fs = require('fs')
-var pwd = function () {
-    process.stdout.write(process.env.PWD);
-    process.stdout.write('\nprompt > ');
+var request = require('request');
+var pwd = function (str, done) {
+    done(process.env.PWD);
 }
 var dt = Date();
-var date = function () {
-    process.stdout.write(dt);
-    process.stdout.write('\nprompt > ');
+var date = function (str, done) {
+    done(dt);
 };
-var ls = function () {
+var ls = function (str, done) {
+    var final = ''
     fs.readdir('.', function (err, files) {
         if (err) throw err;
         files.forEach(function (file) {
-            process.stdout.write(file.toString() + "\n");
+            final += file.toString() + "\n";
         })
-        process.stdout.write('\nprompt > ');
+        done(final)
+
     })
 };
 
-var echo = function (str) {
+var echo = function (str, done) {
     var arr = str.split(' ');
     var strFinal = arr.slice(1)
-    process.stdout.write(strFinal.join(' ').toString());
-    process.stdout.write('\nprompt > ');
+    done(strFinal.join(' ').toString());
 };
 
-var cat = function (str) {
+var cat = function (str,done) {
     var arr = str.split(' ');
     var strFinal = arr.slice(1)
-    var archivo  = strFinal.join(' ').toString()
-    var archivos = archivo.split(' ')    
-    archivos.forEach(function(file){
+    var archivo = strFinal.join(' ').toString()
+    var archivos = archivo.split(' ')
+    archivos.forEach(function (file) {
         fs.readFile(file, 'utf8', (err, data) => {
             if (err) throw err;
-            
-            console.log(data);
-            process.stdout.write('\nprompt > ');        
-          });
+
+            done(data);
+        });
     })
-    
-      
 };
+
+var head = function (str,done) {
+    var final = ''
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = strFinal.join(' ').toString()
+    fs.readFile(archivo, 'utf8', (err, data) => {
+        if (err) throw err;
+        var firstLine = data.split('\n');
+        for (var i = 0; i < 5; i++) {
+            final += firstLine[i] + '\n'
+        }
+        done(final)
+    });
+}
+
+var tail = function (str,done) {
+    var final = ''
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = strFinal.join(' ').toString()
+    fs.readFile(archivo, 'utf8', (err, data) => {
+        if (err) throw err;
+        var firstLine = data.split('\n');
+        for (var i = firstLine.length - 5; i < firstLine.length; i++) {
+            final += firstLine[i] + '\n'
+        }
+        done(final)
+    });
+}
+
+var sort = function (str, done) {
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = strFinal.join(' ').toString()
+    fs.readFile(archivo, 'utf8', (err, data) => {
+        if (err) throw err;
+        var firstLine = data.split('\n');
+        var aux = bubbleSort(firstLine)
+        aux = aux.join('\n');
+        done(aux);
+    });
+}
+
+function bubbleSort(array) {
+    do {
+        var bandera = false
+        var contador = 0
+        for (var i = 0; i < array.length - contador; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i)
+                bandera = true
+                contador++
+            }
+        }
+    }
+    while (bandera)
+    return array
+}
+
+var swap = function (array, i) {
+    var temp = array[i + 1]
+    array[i + 1] = array[i]
+    array[i] = temp
+}
+
+var wc = function (str,done) {
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = strFinal.join(' ').toString()
+    fs.readFile(archivo, 'utf8', (err, data) => {
+        if (err) throw err;
+        var firstLine = data.split('\n');
+        done(firstLine.length.toString())
+    });
+}
+
+var uniq = function (str,done) {
+    var arry = []
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = strFinal.join(' ').toString()
+    fs.readFile(archivo, 'utf8', (err, data) => {
+        if (err) throw err;
+        var firstLine = data.split('\n');
+        for (var i = 0; i < firstLine.length; i++) {
+            if (firstLine[i] !== firstLine[i + 1]) {
+                arry.push(firstLine[i]);
+            }
+        }
+        arry = arry.join('\n');
+        done(arry);
+    });
+}
+
+var curl = function (str, done) {
+    var arry = []
+    var arr = str.split(' ');
+    var strFinal = arr.slice(1)
+    var archivo = 'http://www.' + strFinal.join(' ').toString()
+    request(archivo, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        process.stdout.write('\nprompt > ');
+    });
+}
 
 
 
@@ -49,5 +153,11 @@ module.exports = {
     date: date,
     ls: ls,
     echo: echo,
-    cat:cat
+    cat: cat,
+    head: head,
+    tail: tail,
+    wc: wc,
+    sort: sort,
+    uniq: uniq,
+    curl: curl
 }
